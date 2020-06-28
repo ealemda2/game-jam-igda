f0b76cf39d334a4ed3abf4cb88d3a80f49c1190a
Ertugrul Alemdar <ertalemdar@gmail.com>
2020-06-28 19:00:31 -0400

    Updates

---
+++ precache-manifest.f8bd4dcd1f260c7823be88f82aef6a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bab01364e182591abc7ccc48594ea6",
+    "url": "/ealemda2/ealemda2.github.io.git/index.html"
+  },
+  {
+    "revision": "6da0f195c6648f93d79e",
+    "url": "/ealemda2/ealemda2.github.io.git/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "5631886302eaaa500951",
+    "url": "/ealemda2/ealemda2.github.io.git/static/css/main.f12969e9.chunk.css"
+  },
+  {
+    "revision": "6da0f195c6648f93d79e",
+    "url": "/ealemda2/ealemda2.github.io.git/static/js/2.a5154f4b.chunk.js"
+  },
+  {
+    "revision": "7d2579c8bc39cbb93e4298ffb99d34c7",
+    "url": "/ealemda2/ealemda2.github.io.git/static/js/2.a5154f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5631886302eaaa500951",
+    "url": "/ealemda2/ealemda2.github.io.git/static/js/main.c24ecda9.chunk.js"
+  },
+  {
+    "revision": "c1f465eff0821d345d80",
+    "url": "/ealemda2/ealemda2.github.io.git/static/js/runtime-main.abd9cd6b.js"
+  }
+]);
