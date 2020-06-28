5ba18f3637230257e2738d75210585b2d3927d26
Cem Alemdar <cemalemdar98@gmail.com>
2020-06-28 00:35:31 -0400

    scale bug

--- src/Scene.js
+++ src/Scene.js
@@ -1,5 +1,8 @@
 import React from "react";
-import Matter from "matter-js";
+import Matter, { Mouse } from "matter-js";
+import { getRandomInt } from "./utility"
+import { selectDeselectButton } from "./utility"
+import { vectorLength } from "./utility"
 
 class Scene extends React.Component {
   constructor(props) {
@@ -14,15 +17,20 @@ class Scene extends React.Component {
       Bodies = Matter.Bodies,
       Body = Matter.Body,
       Mouse = Matter.Mouse,
-      MouseConstraint = Matter.MouseConstraint,
-      Common = Matter.Common;
+      MouseConstraint = Matter.MouseConstraint;
+    //Common = Matter.Common;
 
-    let screenSize = {x: window.innerWidth, y: window.innerHeight};
-    // to get a random int in range
-    let getRandomInt = function (min, max) {
-      return parseInt(Math.random() * (max - min) + min);
+    let screenSize = { x: window.innerWidth, y: window.innerHeight };
+    const toolState = {};
+    const setToolState = (key, value) => {
+      if (!Object.keys(toolState).includes(key)) {
+        toolState[key] = null
+      }
+      toolState[key] = value
+      return toolState
     }
 
+    const getToolState = () => toolState
     const engine = Engine.create({
       // positionIterations: 20
     });
@@ -34,20 +42,22 @@ class Scene extends React.Component {
         width: window.innerWidth,
         height: window.innerHeight,
         wireframes: false,
-        background: "#A0F2CF"
+        background: "#E4F5FC",
+        showShadows: true,
+        showDebug: true,
+        showPositions: true,
       },
     });
 
     const ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
     const ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
-  
+
     const wallColor = "#FCFAA4";
     World.add(engine.world, [
       // walls
-      Bodies.rectangle(screenSize.x / 2.0, 0, screenSize.x, 50, { isStatic: true, render: {fillStyle: wallColor} }),              //top wall
-      Bodies.rectangle(screenSize.x / 2, screenSize.y, screenSize.x, 50, { isStatic: true, render: {fillStyle: wallColor} }),     //bottom wall
-      Bodies.rectangle(screenSize.x, screenSize.y / 2.0, 50, screenSize.y, { isStatic: true, render: {fillStyle: wallColor} }),   //right wall
-      Bodies.rectangle(0, screenSize.y / 2.0, 50, screenSize.y, { isStatic: true, render: {fillStyle: wallColor} }),              //left wall
+      Bodies.rectangle(screenSize.x / 2, screenSize.y, screenSize.x, 50, { isStatic: true, render: { fillStyle: wallColor } }),     //bottom wall
+      Bodies.rectangle(screenSize.x, screenSize.y / 2.0, 50, screenSize.y, { isStatic: true, render: { fillStyle: wallColor } }),   //right wall
+      Bodies.rectangle(0, screenSize.y / 2.0, 50, screenSize.y, { isStatic: true, render: { fillStyle: wallColor } }),              //left wall
     ]);
 
     // Head construction
@@ -57,27 +67,27 @@ class Scene extends React.Component {
 
     // Head
     const head = Bodies.circle(screenSize.x / 2.0, screenSize.y / 2.0, 140, {
-      isStatic: true, 
-      render: {fillStyle: headColor}
+      isStatic: true,
+      render: { fillStyle: headColor }
     });
 
     // Nose
     let noseColor = skinColors.splice(getRandomInt(0, skinColors.length - 1), 1);
     let noseSize = 10 * getRandomInt(3, 7);
-    const nose = Bodies.polygon(head.position.x, head.position.y + 20, 3, noseSize, { 
+    const nose = Bodies.polygon(head.position.x, head.position.y + 20, 3, noseSize, {
       angle: Math.PI / 2.0,
-      render: {fillStyle: noseColor},
+      render: { fillStyle: noseColor },
       chamfer: { radius: [20, 0, 20] }
     });
 
     // Eyes
-    let leftEye = Bodies.rectangle(head.position.x - 60, head.position.y - 50, 50, 50, { 
+    let leftEye = Bodies.rectangle(head.position.x - 60, head.position.y - 50, 50, 50, {
       angle: Math.PI / 4.0,
       render: { fillStyle: "#ffffff" },
       chamfer: { radius: [40, 10, 40, 10] }
     });
 
-    let rightEye = Bodies.rectangle(head.position.x + 60, head.position.y - 50, 50, 50, { 
+    let rightEye = Bodies.rectangle(head.position.x + 60, head.position.y - 50, 50, 50, {
       angle: Math.PI / 4.0,
       render: { fillStyle: "#ffffff" },
       chamfer: { radius: [40, 10, 40, 10] }
@@ -87,15 +97,15 @@ class Scene extends React.Component {
     let eyeColorPicker = getRandomInt(0, eyeColors.length - 1);
     console.log(eyeColorPicker);
     // Pupils
-    let rightIris = Bodies.circle(rightEye.position.x, rightEye.position.y, 10, { render: { fillStyle: eyeColors[eyeColorPicker]}});
-    let rightPupil = Bodies.circle(rightEye.position.x, rightEye.position.y, 4, { render: { fillStyle: "#000000" }});
-    let leftIris = Bodies.circle(leftEye.position.x, leftEye.position.y, 10, { render: { fillStyle: eyeColors[eyeColorPicker]}});
-    let leftPupil = Bodies.circle(leftEye.position.x, leftEye.position.y, 4, { render: { fillStyle: "#000000" }});
-    
+    let rightIris = Bodies.circle(rightEye.position.x, rightEye.position.y, 10, { render: { fillStyle: eyeColors[eyeColorPicker] } });
+    let rightPupil = Bodies.circle(rightEye.position.x, rightEye.position.y, 4, { render: { fillStyle: "#000000" } });
+    let leftIris = Bodies.circle(leftEye.position.x, leftEye.position.y, 10, { render: { fillStyle: eyeColors[eyeColorPicker] } });
+    let leftPupil = Bodies.circle(leftEye.position.x, leftEye.position.y, 4, { render: { fillStyle: "#000000" } });
+
 
 
     // Composite object that holds all head parts
-    const fullHead = Body.create({parts: [head, nose, rightEye, leftEye, rightIris, leftIris, rightPupil, leftPupil], isStatic: true})
+    const fullHead = Body.create({ parts: [head, nose, rightEye, leftEye, rightIris, leftIris, rightPupil, leftPupil], isStatic: true })
 
     World.add(engine.world, [ballA, ballB, fullHead]);
 
@@ -113,18 +123,94 @@ class Scene extends React.Component {
 
     World.add(engine.world, mouseConstraint);
 
-    // Mouse input
-    let selectedBody
+    // Button features
+    const buttonStrokeColors = { transform: "#FF0000", scale: "#00FF00", rotate: "#0000FF" };
+    let strokeColor = buttonStrokeColors.transform;
+    let transformButton = Bodies.circle(50, 50, 20, {
+      isStatic: true, isSensor: true, render: {
+        opacity: 1,
+        sprite: {
+          texture: "../images/transform.png",
+          xScale: 1,
+          yScale: 1
+        }
+      }
+    });
+    let rotateButton = Bodies.circle(50, 100, 20, {
+      isStatic: true, isSensor: true, render: {
+        opacity: 1,
+        sprite: {
+          texture: "../images/rotate.png",
+          xScale: 1,
+          yScale: 1
+        }
+      }
+    });
+    let scaleButton = Bodies.circle(50, 150, 20, {
+      isStatic: true, isSensor: true, render: {
+        opacity: 1,
+        sprite: {
+          texture: "../images/scale.png",
+          xScale: 1,
+          yScale: 1
+        }
+      }
+    });
 
+    World.add(engine.world, [scaleButton, transformButton, rotateButton]);
     Matter.Events.on(mouseConstraint, "mousedown", function (event) {
       //const rand = Math.random();
-
-      selectedBody = mouseConstraint.body;
-
-      if(selectedBody && selectedBody.isStatic !== true){
-        selectedBody.render.lineWidth = 10.0;
+      let initialPosition = mouse.position;
+      console.log(toolState.mousePressed);
+      if (!toolState.mousePressed) {
+        setToolState("mouseStartPosition", initialPosition)
+        console.log('mousedown event')
+        console.log(toolState.mouseStartPosition);
+        toolState.mouseDisplacement = 0;
       }
-      
+
+      // deselect previous body
+      if (toolState.selectedBody) {
+        toolState.selectedBody.render.lineWidth = 0;
+      }
+
+      toolState.selectedBody = mouseConstraint.body;
+      toolState.mousePressed = true;
+
+
+
+      // for buttons
+      switch (toolState.selectedBody) {
+        case transformButton:
+          console.log("transform pressed at: " + event.mouse.position);
+          selectDeselectButton(transformButton, [scaleButton, rotateButton]);
+          toolState.currentTool = "transform";
+          strokeColor = buttonStrokeColors.transform;
+          break;
+
+        case rotateButton:
+          console.log("rotate pressed at: " + event.mouse.position);
+          selectDeselectButton(rotateButton, [transformButton, scaleButton]);
+          toolState.currentTool = "rotate";
+          strokeColor = buttonStrokeColors.rotate;
+          break;
+
+        case scaleButton:
+          console.log("scale pressed at: " + event.mouse.position);
+          selectDeselectButton(scaleButton, [transformButton, rotateButton]);
+          toolState.currentTool = "scale";
+          strokeColor = buttonStrokeColors.scale;
+          break;
+
+        default:
+          if (toolState.selectedBody && toolState.selectedBody.isStatic !== true) {
+            toolState.selectedBody.render.lineWidth = 10.0;
+            toolState.selectedBody.render.strokeStyle = strokeColor;
+          }
+          break;
+      }
+
+
       /* Adding more objects by clicking
       if (rand >= 0.5) {
         World.add(
@@ -141,15 +227,53 @@ class Scene extends React.Component {
     });
 
     Matter.Events.on(mouseConstraint, "mouseup", function (event) {
-      if(selectedBody){
-        selectedBody.render.lineWidth = 0.0;
+      /*
+      if(toolState.selectedBody){
+        toolState.selectedBody.render.lineWidth = 0.0;
       }
+      */
+
+      toolState.mousePressed = false;
+    });
+
+    Matter.Events.on(mouseConstraint, "mousemove", function (event) {
+      toolState.mouseCurrentPosition = event.mouse.position;
+
+      if (toolState.mousePressed === true && mouseConstraint.body) {
+        toolState.mouseDisplacement = vectorLength({ x: toolState.mouseCurrentPosition.x - toolState.mouseStartPosition.x, y: toolState.mouseCurrentPosition.y - toolState.mouseStartPosition.y });
+        switch (toolState.currentTool) {
+          case "translate":
+            // just use default constraint for translate
+            break;
+          case "scale":
+
+            if (mouseConstraint.body.isSensor === false || mouseConstraint.body.isStatic === false) {
+              console.log('mousemove event');
+              console.log('mousemove start pos', getToolState().mouseStartPosition);
+              Body.scale(mouseConstraint.body, 1, 1); // scale factor
+            }
+            break;
+          case "rotate":
+            // TODO make object rotate
+            break;
+          default:
+            break;
+        }
+
+
+      }
+    });
+
+    Matter.Events.on(mouseConstraint, "startdrag", function (event) {
+
     });
 
     Engine.run(engine);
     Render.run(render);
 
   }
+
+
 
 
   render() {
---
+++ src/utility.js
@@ -0,0 +1,38 @@
+//import Matter from "matter-js";
+
+export const getRandomInt = (max, min) => {
+    return parseInt(Math.random() * (max - min) + min);
+}
+
+export const selectDeselectButton = (selected, otherButtons) => {
+
+    selected.render.opacity = 0.3;
+    for (let i = 0; i < otherButtons.length; i++) {
+        otherButtons[i].render.opacity = 1;
+    }
+}
+
+//----------------------------------------------------------------------------
+
+export const radians = (degrees) => {
+    return degrees * Math.PI / 180.0;
+}
+
+export const dot = (u, v) => {
+    if (u.length != v.length) {
+        throw { message: "dot(): vectors are not the same dimension" };
+    }
+
+    var sum = 0.0;
+    for (var i = 0; i < u.length; ++i) {
+        sum += u[i] * v[i];
+    }
+
+    return sum;
+}
+
+export const vectorLength = (u) => {
+    return Math.sqrt(dot(u, u));
+}
+
+
